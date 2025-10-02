d7b62986801621035c2e0847b9f2783683921bc8
Harounjlassi <jlassi.Haroun@esprit.tn>
2025-10-02 14:39:18 +0100

    update:1.0.0

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,36 @@
-export default function Results({  input}){
-    console.log(input)
-    return(
-        <p>results ....</p>
-    );
-
-} 
+import { calculateInvestmentResults , formatter} from "../util/investment.js";
+export default function Results({ input }) {
+  //console.log(input);
+  const resultsData = calculateInvestmentResults(input);
+  //console.log(resultsData);
+  const InitialInvestment=resultsData[0].valueEndOfYear-resultsData[0].interest-resultsData[0].annualInvestment;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Intrest (Year)</th>
+          <th>Total Interest </th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const totalInsert=yearData.valueEndOfYear-yearData.annualInvestment* yearData.year-InitialInvestment;
+          const totalAmountInvested=yearData.valueEndOfYear-totalInsert;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInsert)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+           
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
